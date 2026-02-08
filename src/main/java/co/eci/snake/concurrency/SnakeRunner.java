7a408d37ee4c3e4a1730af8d9d31632d2803976d
Christian Alfonso Romero Martinez <christian.romero-m@mail.escuelaing.edu.co>
2026-02-08 18:16:07 -0500

    Add pause support to SnakeRunner

--- src/main/java/co/eci/snake/concurrency/SnakeRunner.java
+++ src/main/java/co/eci/snake/concurrency/SnakeRunner.java
@@ -1,10 +1,11 @@
 package co.eci.snake.concurrency;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import co.eci.snake.core.Board;
 import co.eci.snake.core.Direction;
+import co.eci.snake.core.GameState;
 import co.eci.snake.core.Snake;
-
-import java.util.concurrent.ThreadLocalRandom;
 
 public final class SnakeRunner implements Runnable {
   private final Snake snake;
@@ -13,15 +14,22 @@ public final class SnakeRunner implements Runnable {
   private final int turboSleepMs = 40;
   private int turboTicks = 0;
 
-  public SnakeRunner(Snake snake, Board board) {
+  //Nos da las senales de pausa
+  private final GameState gameState;
+
+  public SnakeRunner(Snake snake, Board board, GameState gameState) {
     this.snake = snake;
     this.board = board;
+    this.gameState = gameState;
   }
 
   @Override
   public void run() {
     try {
       while (!Thread.currentThread().isInterrupted()) {
+
+        //Senal de pausa
+        gameState.awaitIfPaused();
         maybeTurn();
         var res = board.step(snake);
         if (res == Board.MoveResult.HIT_OBSTACLE) {
