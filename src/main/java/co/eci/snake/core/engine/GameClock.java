5d0d405348bf0e05cd97963950b2575276341b67
Christian Alfonso Romero Martinez <christian.romero-m@mail.escuelaing.edu.co>
2026-02-08 18:14:59 -0500

    Separate clock state and integrate game state

--- src/main/java/co/eci/snake/core/engine/GameClock.java
+++ src/main/java/co/eci/snake/core/engine/GameClock.java
@@ -1,35 +1,46 @@
 package co.eci.snake.core.engine;
 
-import co.eci.snake.core.GameState;
-
-import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
+import co.eci.snake.core.GameState;
+
 public final class GameClock implements AutoCloseable {
   private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
   private final long periodMillis;
   private final Runnable tick;
-  private final java.util.concurrent.atomic.AtomicReference<GameState> state = new AtomicReference<>(GameState.STOPPED);
+  private final AtomicReference<ClockState> state = new AtomicReference<>(ClockState.STOPPED);
 
-  public GameClock(long periodMillis, Runnable tick) {
+  private final GameState gameState;
+
+  //El enum ahora esta en local
+  private enum ClockState { STOPPED, RUNNING }
+
+
+  public GameClock(long periodMillis, Runnable tick, GameState gameState) {
     if (periodMillis <= 0) throw new IllegalArgumentException("periodMillis must be > 0");
     this.periodMillis = periodMillis;
     this.tick = java.util.Objects.requireNonNull(tick, "tick");
+    this.gameState = java.util.Objects.requireNonNull(gameState, "gameState");
   }
 
   public void start() {
-    if (state.compareAndSet(GameState.STOPPED, GameState.RUNNING)) {
+    if (state.compareAndSet(ClockState.STOPPED, ClockState.RUNNING)) {
       scheduler.scheduleAtFixedRate(() -> {
-        if (state.get() == GameState.RUNNING) tick.run();
+        if (state.get() == ClockState.RUNNING&& !gameState.isPaused()) {
+          tick.run();
+        }
       }, 0, periodMillis, TimeUnit.MILLISECONDS);
     }
   }
 
-  public void pause()  { state.set(GameState.PAUSED); }
-  public void resume() { state.set(GameState.RUNNING); }
-  public void stop()   { state.set(GameState.STOPPED); }
+  //Game state se encarga de pausar
+  public void pause()  { gameState.pause(); }
+  public void resume() { gameState.resume(); }
+  //public void pause()  { state.set(GameState.PAUSED); }
+  //public void resume() { state.set(GameState.RUNNING); }
+  public void stop()   { state.set(ClockState.STOPPED); }
   @Override public void close() { scheduler.shutdownNow(); }
 }
